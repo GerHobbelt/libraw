e931af58e86e2141a84f8cb2030eb4a6b593192b
Rich Baird <rich@rbaird.me>
2022-10-06 14:09:13 -0600

    use shared lib

--- build-go.sh
+++ build-go.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/env bash
 
 root="$(dirname "$(readlink -f "$0")")"
-rm -rf "$root/build"
+rm -rf "$root/build" "$root/librawgo"
 mkdir "$root/build"
 cd "$root/build" && cmake .. && make
+mv "$root/build/librawgo.go" ./librawgo
---
+++ gotest/go.mod
@@ -0,0 +1,5 @@
+module github.com/MRHT-SRProject/LibRawGoTest
+
+go 1.19
+
+require github.com/MRHT-SRProject/LibRawGo v0.0.6
