ab91a13aad09a0cdb73aae7e32289bfdd4719a0e
Ger Hobbelt <ger@hobbelt.com>
2024-11-17 17:39:09 +0100

    fix potentially infinite loop / out-of-bounds access in LibRaw::remove_trailing_spaces() due to loop test never failing on len dropping below zero: reported by MSVC: identify_tools.cpp(97,30): warning C4296: '>=': expression is always true

--- src/metadata/identify_tools.cpp
+++ src/metadata/identify_tools.cpp
@@ -94,8 +94,9 @@ void LibRaw::remove_trailing_spaces(char *string, size_t len)
   if (len < 3)
     return; // also not needed
   len = strnlen(string, len - 1);
-  for (size_t i = len - 1; i >= 0; i--)
+  for (size_t i = len; i > 0; )
   {
+    --i;
     if (isspace((unsigned char)string[i]))
       string[i] = 0;
     else
